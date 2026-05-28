47dc84b35986ed07916c9937abc3c2f6d2d6ccea
gauravshinde9890-collab <gaurav.shinde.9890@gmail.com>
2026-05-28 11:44:54 +0530

    Update Api.js

--- src/Api.js
+++ src/Api.js
@@ -1,8 +1,7 @@
-src/api.js
 
 import axios from "axios";
 
-export const API_BASE_URL = "https://chat-production-cdc6.up.railway.app";
+export const API_BASE_URL = "https://chat-production-4419.up.railway.app";
 
 const api = axios.create({
   baseURL: API_BASE_URL,
